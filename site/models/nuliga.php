8c29c5069dfb724e99bb1afd4d4b6f28b7315386
Sebastian Schlicht <sebastian@jablab.de>
2017-11-03 14:44:52 +0100

    j3com tutorial part 5

--- site/models/nuliga.php
+++ site/models/nuliga.php
@@ -30,7 +30,19 @@ class NuLigaModelNuLiga extends JModelItem
     {
         if (!isset($this->message))
         {
-            $this->message = 'Hello World!';
+            $jinput = JFactory::getApplication()->input;
+            $id     = $jinput->get('id', 1, 'INT');
+
+            switch ($id)
+            {
+                case 2:
+                    $this->message = 'Good bye World!';
+                    break;
+                default:
+                case 1:
+                    $this->message = 'Hello World!';
+                    break;
+            }
         }
 
         return $this->message;
